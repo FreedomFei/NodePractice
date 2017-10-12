e4cf1b2566b2264bd5f84dd415c540a64ca8213c
wulinfei <wulinfei@nonobank.com>
2017-10-12 17:47:42 +0800

    渠道处理本地temp

--- bldChannel/serverInfo.js
+++ bldChannel/serverInfo.js
@@ -1,7 +1,8 @@
 'use strict'
 
 let os = require('os'),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    opn = require('opn');
 
 //输出url和port并在brower打开
 exports.openUri = function (port) {
@@ -11,16 +12,19 @@ exports.openUri = function (port) {
             if (details.family === 'IPv4' && !details.internal) {
                 var url = 'http://' + details.address + ':' + port;
                 console.log(url);
-                switch (process.platform) {
-                    case "darwin":
-                        exec('open ' + url);
-                        break;
-                    case "win32":
-                        exec('start ' + url);
-                        break;
-                    default:
-                        exec('xdg-open', [url]);
-                }
+                //浏览器打开url方式1，和方式2方法相同
+                opn(url);
+                //浏览器打开url方式2
+                // switch (process.platform) {
+                //     case "darwin":
+                //         exec('open ' + url);
+                //         break;
+                //     case "win32":
+                //         exec('start ' + url);
+                //         break;
+                //     default:
+                //         exec('xdg-open', [url]);
+                // }
             }
         });
     }
